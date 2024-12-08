2f53a77480ce7e2eab259f68a8c28b5bae9ac4c2
Gabriel Nicolosi <gabianp@live.com.ar>
2024-12-08 16:29:21 -0300

    create washingMachine.controller

--- src/controllers/washingMachine.controller.js
+++ src/controllers/washingMachine.controller.js
@@ -2,6 +2,9 @@ import getLogger from "../utils/log.utils.js";
 import * as washingService from "../services/washingMachine.services.js";
 import { sortedProds } from "../helpers/sort.helpers.js";
 import { filteredProds } from "../helpers/filter.helpers.js";
+import { validateproductFormat } from "../helpers/validations.helpers.js";
+import { validateProductFields } from "../helpers/washingMachine.helpers.js";
+import { formatProductData } from "../DTO/washingMachine.dto.js";
 
 const log = getLogger();
 
@@ -69,4 +72,168 @@ const getAllProducts = async (req, res) => {
   }
 };
 
-export { getAllProducts };
+const getProductById = async (req, res) => {
+  // Obtengo el valor del parámetro de ruta 'pid'
+  const id = req.params.pid;
+  try {
+    const product = await washingService.getProductById(id);
+
+    if (!product) {
+      log.error(`Producto con id ${id} no encontrado`);
+      return res.status(400).json({
+        status: "Error",
+        message: "Product not found for bad request",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Producto encontrado",
+      payload: product,
+    });
+  } catch (error) {
+    log.fatal("Error al obtener el producto. " + error.message);
+    res.status(500).json({ status: error, message: "Internal Server Error" });
+  }
+};
+
+const createProduct = async (req, res) => {
+  const data = req.body;
+  //   log.debug("Controller - Data: ", data);
+  try {
+    // Verificar que se proporcione un formato de producto válido y que éste no sea enviado vacío
+    const validatedData = validateproductFormat(data);
+    if (validatedData) {
+      log.error(validatedData);
+      return res.status(400).json({ status: "Error", message: validatedData });
+    }
+
+    // validar campos obligatorios
+    const {
+      marca,
+      modelo,
+      fabricante,
+      tecnologia,
+      carga_tipo,
+      eje,
+      capacidad,
+      rpm_centrif,
+      repuestos,
+      thumbnails,
+    } = data;
+
+    if (
+      !marca ||
+      !modelo ||
+      !tecnologia ||
+      !carga_tipo ||
+      !eje ||
+      !capacidad ||
+      !rpm_centrif
+    ) {
+      log.error(
+        "Error intentando crear el Producto, verificar campos obligatorios"
+      );
+      return res.status(400).json({
+        status: "Error",
+        message: "Bad Request",
+      });
+    }
+
+    // validaciones adicionales
+    const validationError = validateProductFields(data);
+    if (validationError) {
+      log.error(validationError);
+      return res
+        .status(400)
+        .json({ status: "Error", message: validationError });
+    }
+
+    // crear el producto
+    const newProduct = formatProductData(data);
+
+    // Validar que el producto no exista ya en la base de datos
+    const existingProduct = await washingService.findProductByBrandAndModel({
+      marca,
+      modelo,
+    });
+    if (existingProduct) {
+      log.error("El producto ya existe en la base de datos");
+      return res.status(409).json({
+        status: "Error",
+        message: "Conflict: El producto ya existe",
+      });
+    }
+
+    // guardar el producto usando el servicio
+    const createdProduct = await washingService.createProduct(newProduct);
+
+    res.status(201).json({
+      status: "success",
+      message: "Producto creado correctamente",
+      payload: createdProduct,
+    });
+  } catch (error) {
+    log.fatal(`Error al guardar el producto: ${error.message}`, { error });
+    return res
+      .status(500)
+      .json({ status: error, message: "Internal Server Error" });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  const id = req.params.pid;
+  const data = req.body;
+
+  try {
+    // Verificar que se proporcione un formato de producto válido y que éste no sea enviado vacío
+    const validatedData = validateproductFormat(data);
+    if (validatedData) {
+      log.error(validatedData);
+      return res.status(400).json({ status: "Error", message: validatedData });
+    }
+
+    // enviar la data
+    const updatedProduct = await washingService.updateProductById(id, data);
+
+    res.status(200).json({
+      status: "success",
+      message: "Producto actualizado correctamente",
+      payload: updatedProduct,
+    });
+  } catch (error) {
+    log.fatal("Error al actualizar el producto. " + error.message);
+    res.status(500).json({ status: "Error", message: "Internal Server Error" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const id = req.params.pid;
+
+  try {
+    const prodToDelete = await washingService.getProductById(id);
+    if (!prodToDelete) {
+      log.error("Producto no encontado");
+      return res
+        .status(404)
+        .json({ status: "Error", message: "Product not found" });
+    }
+
+    await washingService.deleteProduct(id);
+    return res.status(200).json({
+      status: "success",
+      message: "El recurso ha sido eliminado correcatamente",
+    });
+  } catch (error) {
+    log.fatal("Error al eliminar el producto. " + error.message);
+    res.status(500).json({ status: "Error", message: "Internal Server Error" });
+  }
+};
+
+export {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
